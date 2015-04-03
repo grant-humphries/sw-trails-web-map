96bf7f31a9510314b80e94138189fd660b280a84
grant-humphries <grant.humphries@gmail.com>
2015-04-02 22:35:33 -0700

    finished script to grab osm from overpass api and convert to geojson

--- bin/create_geojson_from_osm.sh
+++ bin/create_geojson_from_osm.sh
@@ -1,34 +1,39 @@
 # !/bin/sh
-# 
+
+# get OSM trail data from the overpass api as .osm and convert it to geojson
+# using ogr2ogr
 
 project_dir="/Users/granthumphries/Coding/git/sw-trails-web-map"
 
 # bounding box should be feed to overpass in the following order
-# min lat, min lon, max lat, max lon (or S-W-N-E)
+# min-lat,min-lon,max-lat,max-lon (or S,W,N,E)
 
 min_lat="45.433144"
 min_lon="-122.763594"
 max_lat="45.5177"
 max_lon="-122.639207"
 
-getOsmJsonFromOverpass() {
-	osm_geojson="${project_dir}/test.geojson"
+osm_file="${project_dir}/sw_trails.osm"
+geojson_file="${project_dir}/sw_trails.geojson"
 
+getOsmJsonFromOverpass() {
 	overpass_url="http://overpass-api.de/api/interpreter?data="
-	tags="[highway=\"footway\"]"
+	tags="[highway~\"footway|path|cycleway|pedestrian|steps\"]"
 	bbox="(${min_lat}, ${min_lon}, ${max_lat}, ${max_lon})"
 
-	echo "wget -O $osm_geojson ${overpass_url}${format};way${tags}${bbox};out body;>;out skel qt;"
-	wget -O $osm_geojson "${overpass_url};way${tags}${bbox};out body;>;out skel qt;"
+	# docs on overpass query language here:
+	# http://wiki.openstreetmap.org/wiki/Overpass_API/Overpass_QL
+	# the phrase '(._;>;)' takes the result of the first part pf the query, the
+	# way with out its nodes, recurses down and gets the nodes that belong to the
+	# way and then unions them (that's what the parens do)
+	echo "wget -O $osm_file ${overpass_url}way${tags}${bbox};(._;>;);out body;"
+	wget -O $osm_file "${overpass_url}way${tags}${bbox};(._;>;);out body;"
 }
 
-#getOsmJsonFromOverpass;
+osmToGeojson() {
+	echo "ogr2ogr -f GeoJSON $geojson_file $osm_file lines"
+	ogr2ogr -f GeoJSON $geojson_file $osm_file lines
+}
 
-wget -O "new_test.geojson" "http://overpass-api.de/api/interpreter?data=[out:json][timeout:25];(way[\"highway\"=\"footway\"](45.49208889579099,-122.70986080169678,45.50326398859537,-122.69288778305055););out body;>;out skel qt;"
-
-# WHAT TO DO NEXT
-# geojson is different than json, so first .osm must be downloaded from overpass
-# https://help.openstreetmap.org/questions/24126/overpassapi-different-json-formats
-
-# then the convert osm must be converted to geojson
-# http://gis.stackexchange.com/questions/22788/how-do-you-convert-osm-xml-to-geojson
+getOsmJsonFromOverpass;
+osmToGeojson;
